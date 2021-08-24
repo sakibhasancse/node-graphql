309eadd633c7358a5a5279746820d38e9896fc62
SakibHasanCse <sakibhasan9641@gmail.com>
2021-08-24 23:34:28 +0600

    added a typeDefs

--- src/typeDefs/schema.js
+++ src/typeDefs/schema.js
@@ -2,8 +2,22 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
     type Student{
+        id: ID!,
+        email: String!
+        fullName: String!
+        dep: String!
+        enrolled: Boolean!
+    }
+    type Query{
+        enrollment: [Student!],
+        students: [Student!,
+        student(id: ID!)]: Student
+    }
 
-
+    type Mutation{
+        registerStudent(email: String!, fullName: String!, dept: String ) : String!
+        enroll(id: ID!): String
     }
 
 `
+module.exports = typeDefs
